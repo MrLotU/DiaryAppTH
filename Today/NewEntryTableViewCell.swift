dbdc90f8030b77fc191b9e2180e6572d73c99ad9
MrLotU <j.koopman@jarict.nl>
2017-09-22 10:52:02 +0200

    Added new post stuffage, fixed error

---
+++ Today/NewEntryTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  NewEntryTableViewCell.swift
+//  Today
+//
+//  Created by Jari Koopman on 22/09/2017.
+//  Copyright © 2017 JarICT. All rights reserved.
+//
+
+import UIKit
+
+class NewEntryTableViewCell: UITableViewCell {
+
+
+}
--- Today/NewPostTableViewController.swift
+++ Today/NewPostTableViewController.swift
@@ -10,34 +10,19 @@ import UIKit
 
 class NewPostTableViewController: UITableViewController {
     
+    lazy var dataSource: NewEntryDataSource = {
+        return NewEntryDataSource(fetchRequest: Entry.allEntriesRequest, tableView: self.tableView)
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let barButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButtonPressed))
+        self.navigationItem.setRightBarButton(barButtonItem, animated: false)
+        self.tableView.dataSource = dataSource
+        self.tableView.delegate = dataSource
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 2
-    }
-
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "newEntryCell", for: indexPath)
-        return cell
-//        switch (indexPath.section, indexPath.row) {
-//        case (0, 0):
-//            let cell = tableView.dequeueReusableCell(withIdentifier: "newEntryCell", for: indexPath)
-//            return cell
-//        default:
-//            let cell = tableView.dequeueReusableCell(withIdentifier: "entryCell", for: indexPath)
-//            return cell
-//        }
+    func doneButtonPressed() {
+        
     }
 }
